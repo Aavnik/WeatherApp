55b184a94376b35cd6c38efd12f7dcd355fec13a
Aavnik5 <aav5roy@gmail.com>
2021-04-06 13:55:23 +0530

    add heroku

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'k3_wdtf!r(ww7+87tne2^7p80)c^**t!dlsmi6)ds6yfc0a)9-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
