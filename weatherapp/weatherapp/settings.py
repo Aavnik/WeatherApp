22a750054da00252fcd43014f4032f1765520e0f
Aavnik5 <aav5roy@gmail.com>
2021-04-06 18:27:55 +0530

    add heroku

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'k3_wdtf!r(ww7+87tne2^7p80)c^**t!dlsmi6)ds6yfc0a)9-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,5 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_URL = '/static/weatherapp'
+STATIC_ROOT = 'staticfiles'
+
